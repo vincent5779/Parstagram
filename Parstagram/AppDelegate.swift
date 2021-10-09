583004f0bd364710ce6e20eecd81477ccac970fd
vincent5779 <vncent5779@gmail.com>
2021-10-09 15:17:34 -0500

    unit5 step 3 finished with connection issue
    back4app connection issue. appdelegate.swift need to check the connection issue

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "2OZGio7XeRkUOfniOOMXebGgyDblTm8vMX8STtC9"
+            $0.clientKey = "kSSqkV3lgCOww746fV9tUQ9sWbLUbqg04PIKWzy6"
+            $0.server = "http://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  
+  # Pods for MyApp
+  # This pulls in the latest version of Alamofire. If you have an older version of Xcode, please user version 4.0 or check their documentation!
+  pod 'Parse'
+  pod 'AlamofireImage'
+
+  # Pods for Parstagram
+
+end
